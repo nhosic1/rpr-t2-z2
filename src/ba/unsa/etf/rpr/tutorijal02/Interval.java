eb9902dbd5a7a6aef6029bae1ce7999fc10735e9
nedim <https://github.com/nhosic1>
2020-11-06 14:48:00 +0100

    Dodani atributi, konstruktor, geteri i seteri klase Interval

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,47 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
+    private double pocetnaTacka;
+    private double krajnjaTacka;
+    private boolean pocetnaPripada;
+    private boolean krajnjaPripada;
 
+    public Interval(double pocetnaTacka, double krajnjaTacka, boolean pocetnaPripada, boolean krajnjaPripada) {
+        this.pocetnaTacka = pocetnaTacka;
+        this.krajnjaTacka = krajnjaTacka;
+        this.pocetnaPripada = pocetnaPripada;
+        this.krajnjaPripada = krajnjaPripada;
+    }
 
+    public double getPocetnaTacka() {
+        return pocetnaTacka;
+    }
+
+    public void setPocetnaTacka(double pocetnaTacka) {
+        this.pocetnaTacka = pocetnaTacka;
+    }
+
+    public double getKrajnjaTacka() {
+        return krajnjaTacka;
+    }
+
+    public void setKrajnjaTacka(double krajnjaTacka) {
+        this.krajnjaTacka = krajnjaTacka;
+    }
+
+    public boolean isPocetnaPripada() {
+        return pocetnaPripada;
+    }
+
+    public void setPocetnaPripada(boolean pocetnaPripada) {
+        this.pocetnaPripada = pocetnaPripada;
+    }
+
+    public boolean isKrajnjaPripada() {
+        return krajnjaPripada;
+    }
+
+    public void setKrajnjaPripada(boolean krajnjaPripada) {
+        this.krajnjaPripada = krajnjaPripada;
+    }
 }
